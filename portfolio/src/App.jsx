d743ccf74c87b09c6573a0f650b97ca099683368
Keegan Hinton <khinton386@gmail.com>
2024-11-10 23:10:26 -0500

    fixed functionality of application

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/header'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Portfolio from './pages/portfolio'
-import Footer from './components/footer'
-import './App.css'
-import './styles/resetStyles.css'
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/about';
+import Header from './components/header';
+import Footer from './components/footer';
 function App() {
   return (
     <Router>
-      <Header />
-      
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-      
-      <Footer />
+      < Header />
+      <main>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </main>
+      < Footer />
     </Router>
   );
 }
 
-
-export default App
+export default App;
