2fe5ddfc0df74ad63fa036e272af3eefc20f4162
Keegan Hinton <khinton386@gmail.com>
2024-11-10 20:07:36 -0500

    started the about file in pages

---
+++ portfolio/src/pages/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section>
+            <h1>About Me</h1>
+            <h2>Keegan Hinton</h2>
+            <p>
+                Hi, I'm Keegan. I'm a software engineer and I love React.
+            </p>
+        </section>
+    );
+}
+
+export default About;
