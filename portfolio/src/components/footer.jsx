7457f6e794f668da63d87ee85d6cc3b9bcdca3e3
Keegan Hinton <khinton386@gmail.com>
2024-11-12 00:11:08 -0500

    added in some styling to the webpage

--- portfolio/src/components/footer.jsx
+++ portfolio/src/components/footer.jsx
@@ -1,19 +1,18 @@
 import React from  'react'
 import { FaGithub} from 'react-icons/fa'
+import '../styles/footer.css'
 
 function Footer() {
     return (
-        <footer>
-            <div>
-                <ul>
-                    <li>
-                        <a href="https://github.com/soibun-sol" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                            <FaGithub className="footer-icon" />
-                        </a>
-                    </li>
+        <footer className= "footer" >
+            <div className= "footer-content">
+                <ul className= "footer-links">
+                    <a href="https://github.com/soibun-sol" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                        <FaGithub className="footer-icon" />
+                    </a>
                 </ul>
             </div>
-            <p>&copy; 2024 Keegan Hinton. All rights reserved.</p>
+            <p className= "footer-text">&copy; 2024 Keegan Hinton. All rights reserved.</p>
         </footer>
     );
 }
